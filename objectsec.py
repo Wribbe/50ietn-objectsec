aae22a4db837d958e7bf7eb42f3c19bc28d9aaff
wribbe <eng_steff@hotmail.com>
2017-09-19 20:05:25 +0200

    Generating MAC works, testing different ways of destroying integrity.

--- objectsec.py
+++ objectsec.py
@@ -4,8 +4,10 @@ import random
 import socket
 import sys
 
-from Crypto.Cipher import AES
 from Crypto import Random
+
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC
 
 PORT = 12000
 HOST = ''
@@ -19,7 +21,6 @@ CMD_SHARED = "SHARED"
 
 DELIM_DATA = ','
 ENCODING='utf-8'
-
 
 """
 Proof-of-concept secure end-to-end communication using object security.
@@ -90,14 +91,26 @@ def handshake(shake_type, socket, address):
         print("Keeping {} as own secret.".format(a))
         return p, g, g_raised_to_a_mod_p, a
 
-def send_data(socket, data, address, key=""):
+def send_data(socket, data, address, key="", poison="",
+        poison_message=False):
     if type(data) != list:
         data = [data]
     data = [str(item) for item in data]
-    data = bytes(",".join(data), ENCODING)
     if key:
+        # Generate MAC and prefix to data.
+        mac = generate_mac(key, data)
+        if poison == "MAC": # Remove first element of mac.
+            mac = mac[1:]
+        elif poison == "DATA_PLAIN": # Remove first element in first data.
+            data[0] = data[0][1:]
+        # Prepend MAC to data.
+        data = [mac] + data
+    data = bytes(",".join(data), ENCODING)
+    if key: # Encrypt data.
         data = encrypt(key, data)
-        print("Sending encrypted data:", data)
+        if poison == "DATA_ENCRYPTED":
+            # Remove last encrypted token.
+            data = data[:len(data)-1]
     socket.sendto(data, address)
 
 def get_data(socket, key=""):
@@ -107,6 +120,14 @@ def get_data(socket, key=""):
         data = decrypt(key, data)
     str_data = data.decode(ENCODING)
     str_tokens = [token.strip() for token in str_data.split(DELIM_DATA)]
+    if key:
+        print("Checking MAC..")
+        recieved_mac = str_tokens.pop(0)
+        calculated_mac = generate_mac(key, str_tokens)
+        if recieved_mac == calculated_mac:
+            print("MAC checks out, integrity of data is ok!")
+        else:
+            print("MAC did not match, integrity of data can not be verified!")
     return str_tokens, sender
 
 def encrypt(key, message):
@@ -129,6 +150,13 @@ def calculate_shared_secret(computed_primitive, own_secret, public_mod):
     shared_secret = (str(shared_secret)*10)[:secret_length]
     # Convert back to bytes and return.
     return bytes(shared_secret, ENCODING)
+
+def generate_mac(key, message):
+    message = bytes(str(message), ENCODING)
+    hmac = HMAC.new(key)
+    hmac.update(message)
+    # Return printable MAC.
+    return hmac.hexdigest()
 
 def server():
 
@@ -158,6 +186,7 @@ def server():
             print("Have the shared secret: {}".format(shared_rsa_secret))
         else: # Message.
             print("Got message: {}".format(tokens))
+        print()
 
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -192,12 +221,32 @@ def client():
         return shared_rsa_secret
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+
         shared_rsa_secret = handshake(address)
         print("Computed shared secret: {}".format(shared_rsa_secret))
         message_plaintext = "This is an encrypted message."
         print("Massage plain-text:", message_plaintext)
         send_data(s, message_plaintext, address, shared_rsa_secret)
+        print()
 
+        message_mac_poison = "Message with poisoned MAC."
+        print("Sending message with poisoned MAC:", message_mac_poison)
+        send_data(s, message_mac_poison, address, shared_rsa_secret,
+                poison="MAC")
+        print()
+
+        message_ok_mac_bad_message = "!Defect message with ok MAC."
+        print("Sending bad message with ok MAC:", message_ok_mac_bad_message)
+        send_data(s, message_ok_mac_bad_message, address, shared_rsa_secret,
+                poison="DATA_PLAIN")
+        print()
+
+        message_poisoned_encryption = "Ok message with ok MAC but damaged"+\
+                                      " encryption!"
+        print("Sending ok message with ok MAC:", message_poisoned_encryption)
+        send_data(s, message_poisoned_encryption, address, shared_rsa_secret,
+                poison="DATA_ENCRYPTED")
+        print()
 
 def main(args):
     usage = "Usage: [python] {} --client/--server"
